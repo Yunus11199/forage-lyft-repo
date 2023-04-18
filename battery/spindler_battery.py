1b3da170202e144e328a201353b9a1bbf2c6c67c
Yunus Khan <Yunus111.yk@googlemail.com>
2023-04-18 17:43:13 +0100

    Post Task 2

---
+++ battery/spindler_battery.py
@@ -0,0 +1,18 @@
+from abc import ABC
+from car import Car
+from datetime import datetime
+
+
+
+class SpindlerBattery(Car, ABC):
+    def __init__(self, last_service_date):
+        super().__init__(last_service_date)
+        self.service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        self.current_date = datetime.today().date()
+
+    def battery_should_be_serviced(self):
+        if self.service_threshold_date.year < self.current_date.year:
+            return True
+        else:
+            return False
+
--- test/test_car.py
+++ test/test_car.py
@@ -1,11 +1,11 @@
 import unittest
 from datetime import datetime
 
-from engine.model.calliope import Calliope
-from engine.model.glissade import Glissade
-from engine.model.palindrome import Palindrome
-from engine.model.rorschach import Rorschach
-from engine.model.thovex import Thovex
+from model.calliope import Calliope
+from model.glissade import Glissade
+from model.palindrome import Palindrome
+from model.rorschach import Rorschach
+from model.thovex import Thovex
 
 
 class TestCalliope(unittest.TestCase):
@@ -91,7 +91,7 @@ class TestPalindrome(unittest.TestCase):
 
     def test_battery_should_not_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 2)
         warning_light_is_on = False
 
         car = Palindrome(last_service_date, warning_light_is_on)
